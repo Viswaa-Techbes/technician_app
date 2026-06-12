98318bd87ed6d919393138e3d0c2927d45430a72
Viswaa-Techbes <viswaa-e@techbes.co.in>
2026-06-12 15:20:23 +0530

    cust delete

--- customer_app/lib/features/payment/providers/payment_provider.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:customer_app/features/payment/repositories/payment_repository.dart';
-
-final paymentRepositoryProvider = Provider<PaymentRepository>((ref) {
-  return PaymentRepository();
-});
