530f6b991eaa7e13b74d42dcbf4d946fbe78d7d0
Viswaa-Techbes <viswaa-e@techbes.co.in>
2026-07-07 09:47:03 +0530

    feat: implement v2 customer routes for wallet, tickets, and dashboard statistics

--- BE/routes/v2/customerRoutesV2.js
+++ BE/routes/v2/customerRoutesV2.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const customerController = require('../../controllers/v2/customerControllerV2');
-const { authenticate } = require('../../middlewares/authMiddleware');
+const { authenticate } = require('../../middlewares/auth');
 
 router.use(authenticate);
 
